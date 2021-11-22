cc287c751dc9ab5d306ebc89c7ac08f0f4d04db9
Suraj J Unni <surajjunni52@gmail.com>
2021-11-22 22:38:50 +0530

    Update task.py
    Added docstrings

--- wikiapp/task.py
+++ wikiapp/task.py
@@ -1,19 +1,21 @@
 import string
 from .models import Continent,Country,City
 from celery import shared_task
-
+""" async Continent creation and updation """
 @shared_task
 def create_continent(data):
     print(data)
     Continent.objects.create(name=data['name'], population=data['population'], area_in_sq_meters=data['area_in_sq_meters'])
     return '{} continent data created with success!'.format (data)
 
+""" async Country creation and updation """
 @shared_task
 def create_country(data):
     print(data)
     Country.objects.create(name=data['name'], population=data['population'], area_in_sq_meters=data['area_in_sq_meters'],number_of_hospitals=data['number_of_hospitals'],number_of_national_parks=data['number_of_national_parks'],continent=data['continent'])
     return '{} country data created with success!'.format (data)
 
+""" async City creation and updation """
 @shared_task
 def create_city(data):
     print(data)
